f7ba14ab840e4b5d80cb5e349319879c25ba74a0
flamingo_o <vedi05962@gmail.com>
2025-09-30 21:10:35 +0530

    08: trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,16 +3,17 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+const Page = async () => {
 
-const Page =async() =>{
     const session = await auth.api.getSession({
         headers: await headers(),
     });
-    if(!session){
+    if (!session) {
         redirect('/sign-in');
     }
-    return(
-        <HomeView/>
+
+    return (
+        <HomeView />
     );
 }
 export default Page;
