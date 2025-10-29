82a0dd2f4b783c9ebb25c29cbf149e3336b50e75
flamingo_o <vedi05962@gmail.com>
2025-10-29 22:26:25 +0530

    16: Meeting Setup

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,7 +3,7 @@ import {db} from "@/db";
 import { agents } from "@/db/schema";
 import { agentsInsertSchema, agentsUpdateSchema } from "../schema";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
-import { baseProcedure, createTRPCRouter,protectedProcedure } from "@/trpc/init";
+import { createTRPCRouter,protectedProcedure } from "@/trpc/init";
 import { MIN_PAGE_SIZE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, DEFAULT_PAGE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 
