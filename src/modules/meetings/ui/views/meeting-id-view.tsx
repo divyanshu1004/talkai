1957b92d1c3d6c291512e1e1d662cab2a3d5ad59
flamingo_o <vedi05962@gmail.com>
2025-11-25 03:30:05 +0530

    25: Completed State

--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -12,6 +12,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
+import { CompletedState } from "../components/completed-state";
 
 
 
@@ -75,7 +76,7 @@ export const MeetingIdView = ({ meetingId }: Props) => {
                 onRemove={handleRemoveMeeting}
             />
             {isCancelled && <CancelledState />}
-            {isCompleted && <div>Completed</div>}
+            {isCompleted && <CompletedState data={data} />}
             {isProcessing && <ProcessingState />}
             {isActive && <ActiveState meetingId={meetingId}/>}
             {isUpcoming && <UpcomingState
