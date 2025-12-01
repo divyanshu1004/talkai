7de37aaf90b1aa9b64c2471ff9ec46c800ec5116
flamingo_o <vedi05962@gmail.com>
2025-12-01 18:36:13 +0530

    28: Bugs

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,21 +2,18 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-
-const Layout = ({ children }: Props) =>{
-    return (
-        <SidebarProvider>
-            <DashboardSidebar />
-            <main className="flex flex-col h-screen w-screen bg-muted">
-                <DashboardNavbar />
-                {children}
-            </main>
-            
-        </SidebarProvider>
-        
-    );
-}
+const Layout = ({ children }: Props) => {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main className="flex flex-col min-h-screen w-full bg-muted">
+        <DashboardNavbar />
+        <div className="flex-1 overflow-y-auto">{children}</div>
+      </main>
+    </SidebarProvider>
+  );
+};
 export default Layout;
